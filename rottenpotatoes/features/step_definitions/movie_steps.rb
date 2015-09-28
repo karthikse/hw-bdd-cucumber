a545523d2873a12e8f01a6485e4b5352155e7524
karthikse <karthik2014mays@tamu.edu>
2015-09-27 23:45:31 -0300

    hw3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,6 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
   #fail "Unimplemented"
 end
@@ -14,6 +15,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
+  page.body.index(e1).should < page.body.index(e2)
   #fail "Unimplemented"
 end
 
@@ -25,10 +27,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(", ")
+  ratings.each do |rating|
+    if uncheck
+      step "I uncheck \"ratings_" + rating + "\""
+    else
+      step "I check \"ratings_" + rating + "\""
+    end
+  end
   #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
+  false
   #fail "Unimplemented"
 end
